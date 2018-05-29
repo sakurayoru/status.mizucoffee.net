17cb8abeb1aeb205f4fdeca049db06a55ceefff4
Ritsuki Goto <develop@mizucoffee.net>
2018-05-30 03:11:15 +0900

    [add] graph

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 'use strict'
 
 const express = require('express')
+const fs = require('fs-extra')
+const CronJob = require('cron').CronJob
+const request = require('sync-request')
 const exec = require('child_process').spawnSync;
 const app = express()
 const server = app.listen(process.env.PORT, () => console.log("Node.js is listening to PORT:" + server.address().port))
+
+const ping = new CronJob({
+  cronTime: "0 * * * *",
+  onTick: () => {
+    let start_ms = new Date().getTime()
+    request('GET', 'https://twiback.mizucoffee.net/');
+    let elapsed_ms = new Date().getTime() - start_ms;
+    let ping = fs.readJsonSync('./ping.json')
+    ping.pop()
+    ping.unshift(elapsed_ms)
+    fs.writeJsonSync('./ping.json', ping)
+  },
+  start: false,
+  timeZone: "Asia/Tokyo"
+})
+
+ping.start()
 
 app.disable('x-powered-by')
 app.use(express.static('./public'))
@@ -11,6 +31,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
+  let ping = fs.readJsonSync('./ping.json')
   let neofetch = exec('neofetch', ['--stdout']).stdout.toString().replace(/\n/g,'<br>')
   let nodejs = exec('node', ['-v']).stdout.toString()
   let java = exec('java', ['-version']).stderr.toString().split('\n')[0]
@@ -18,6 +39,7 @@ app.get('/', (req, res) => {
   res.render('index',
     {
       neofetch: neofetch,
+      data: ping,
       versions: {
         nodejs: nodejs,
         java: java
