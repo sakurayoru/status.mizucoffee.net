4d85a6ff365dc778fdd08182de6f858b157979c8
Shun <sakurayoru@localhost.localdomain>
2020-03-22 20:43:43 +0900

    [change]

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra')
 const request = require('sync-request')
 const exec = require('child_process').spawnSync;
 const app = express()
-const server = app.listen(process.env.PORT, () => console.log("Node.js is listening to PORT:" + server.address().port))
+const server = app.listen(process.env.PORT || 24742, () => console.log("Node.js is listening to PORT:" + server.address().port))
 
 app.disable('x-powered-by')
 app.use(express.static('./public'))
@@ -13,23 +13,23 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-  let neofetch = exec('neofetch').stdout.toString().replace(/\n/g,'<br>')
+  let neofetch = exec('neofetch', ['--stdout']).stdout.toString().replace(/\n/g,'<br>')
   let nodejs = exec('node', ['-v']).stdout.toString() .replace(/\n/g,'<br>')
-  let pm2 = exec('pm2', ['monit']).stdout.toString()
+  let pm2 = exec('pm2', ['list']).stdout.toString().replace(/\n/g,'<br>')
   //  let java = exec('java', ['-version']).stderr.toString().split('\n')[0]
 
   res.render('index',
     {
       neofetch: neofetch,
+      pm2: pm2,
       versions: {
         nodejs: nodejs
     //    java: java
       }
-      pm2 : pm2
     });
 });
 
-// nodeバージョン
 // neofetch
+// pm2 list
+// nodeバージョン
 // java
-// pm2 monit
